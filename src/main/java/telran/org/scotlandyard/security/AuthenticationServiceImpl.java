a14fb92063280d4cf32400541d8c3738769cd80d
Eugen <eugen.volkau@gmail.com>
2024-08-09 19:57:51 +0200

    for sequrity installation

---
+++ src/main/java/telran/org/scotlandyard/security/AuthenticationServiceImpl.java
@@ -0,0 +1,17 @@
+package telran.org.scotlandyard.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import telran.org.scotlandyard.security.modele.JwtAuthenticationResponse;
+import telran.org.scotlandyard.security.modele.SignInRequest;
+
+public class AuthenticationServiceImpl implements AuthenticationService{
+
+
+    @Override
+    public JwtAuthenticationResponse authenticate(SignInRequest request) {
+        @Autowired
+                private AuthenticationManager
+
+        return null;
+    }
+}
