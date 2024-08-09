38627018c63850c0cbdc2de9a2dafa7f56ef852f
Eugen <eugen.volkau@gmail.com>
2024-08-09 19:57:22 +0200

    for sequrity installation

--- src/main/java/telran/org/scotlandyard/controller/UserController.java
+++ src/main/java/telran/org/scotlandyard/controller/UserController.java
@@ -5,20 +5,36 @@ import org.springframework.web.bind.annotation.*;
 import telran.org.scotlandyard.entity.User_out;
 import telran.org.scotlandyard.service.UserService;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("User_out")
+@RequestMapping("/user_out")
 @RequiredArgsConstructor
 public class UserController {
 
-    private final UserService userService;
+    public final UserService userService;
 
    @PostMapping
    public User_out create(@RequestBody User_out user_out){
+
        return  userService.create(user_out);
    }
+   @GetMapping
+    public List<User_out> getAll(){
+       return userService.getAll();
+    }
    @GetMapping("/{id}")
     public User_out getById(@PathVariable Long id){
+
        return userService.getById(id);
    }
+     @GetMapping("/search")
+    public User_out findByEmail(@RequestParam String email){
+       return userService.findByEmail(email);
+   }
+@DeleteMapping
+    public void delete(@RequestParam String email){
+       userService.deleteByEmail(email);
+}
 
 }
